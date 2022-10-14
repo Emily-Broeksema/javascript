030aa62b7e2aaafb9f238ff3b3dcacc62b551b6a
Emily-Broeksema <9017781@student.zadkine.nl>
2022-10-14 11:03:39 +0200

    opdracht 6.1 kleur aanpassen

---
+++ bam/script.js
@@ -0,0 +1,7 @@
+function ChangeColor() {
+    alert("De kleur wordt aangepast");
+    console.log("Het werkt ook in de console!");
+    document.getElementById("tekst1").style.color = "red";
+}
+
+function Calculator()
