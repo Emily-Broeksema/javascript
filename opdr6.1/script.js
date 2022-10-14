a2120c29c8aa8f13265e60de68d0224bbbf6b7ec
Emily-Broeksema <9017781@student.zadkine.nl>
2022-10-14 13:17:31 +0200

    opdracht 6.1 en 6.2

---
+++ opdr6.1/script.js
@@ -0,0 +1,11 @@
+function KleurAanpassen(){
+
+alert("De tekst en kleur wordt aangepast.")
+console.log("Het werkt ook in de console!")
+
+
+document.getElementById("tekst1").style.color = "red";
+
+document.getElementById("tekst1").innerHTML = "Inhoud aangepast";
+
+}
