7b539f55b4a65b9625e99b9d19f5b928a40efe00
Emily-Broeksema <9017781@student.zadkine.nl>
2022-10-18 14:29:17 +0200

    a

---
+++ opdracht_6/opdr_6.4/script.js
@@ -0,0 +1,14 @@
+function calculator(num, num2) {
+    let answer = num + num2;
+    console.log(10);
+}
+
+function multiplyByFive(num, num2) {
+    let answer = num * num2;
+    console.log(35);
+}
+
+function calculateMinutes(num, num2) {
+    let answer = num / num2;
+    console.log(6)
+}
